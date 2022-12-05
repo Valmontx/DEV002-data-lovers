b64ff4caeddc3f5cdbd812982ad640f20d557d3d
maryyfry <imarione.tt@gmail.com>
2022-12-05 08:46:47 -0600

    header responsive

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-    
-    let login = document.getElementById('#enviar');
-    login.addEventListener("click",() =>{
-        let name = document.getElementById("nombre").value;
-        localStorage.setItem(name);
-    
-        window.location="otro.html";
-    })
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+// // import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
 
 
 
 
-
-
-
-//console.log(example, data);
