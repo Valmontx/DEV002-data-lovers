e6c7bbaab3c60c46afb00d95053db1f34680eeac
maryyfry <imarione.tt@gmail.com>
2022-12-06 20:01:16 -0600

    Se muestra data con poster y año

--- src/main.js
+++ src/main.js
@@ -4,7 +4,23 @@ import { example } from './data.js';
 // // import data from './data/rickandmorty/rickandmorty.js';
 import data from './data/ghibli/ghibli.js';
 
+/*-------------------Mostrar data-----------------------*/
 
+let cards = document.getElementById("cards")
+data.films.forEach(element => {
 
+    let newDiv = document.createElement("div")
+    newDiv.className = "card"
+    cards.appendChild(newDiv)
 
+    newDiv.innerHTML += `
+    
+    <div class="poster"><img id="poster" src="${element.poster}"/></div>
+    <p class ="parrafo" id="title" class="title">  ${element.title} </p>
+    <p class ="parrafo" id="releaseDate">  ${element.release_date} </p>
+    
+    </div>
+    </div>
+     `
 
+})
